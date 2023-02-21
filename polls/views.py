e866276b211b18a6e7413d93002f0fa8e11cbecd
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-21 13:19:53 +0200

    Installed django-debug-toolbar

--- polls/views.py
+++ polls/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
         'latest_question_list': latest_question_list
     }
