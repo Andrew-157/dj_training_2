f6a778701b11dea7f23099fac6c985859555753f
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-21 10:59:46 +0200

    Added string representation to models

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,17 @@ from django.db import models
 
 
 class Question(models.Model):
-    question = models.CharField(max_length=255)
+    question_text = models.CharField(max_length=255)
     pub_date = models.DateTimeField('date_published')
+
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.CharField(max_length=255)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.question
