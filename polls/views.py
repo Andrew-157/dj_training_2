3499b6af7c249cb32a655705b344d594524cfb36
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-21 12:13:14 +0200

    added some dummy views in polls

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,18 @@
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse('Hello, this is polls')
+    return HttpResponse(f"Hello, this is polls index page")
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of the question {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting for the question {question_id}')
