2355b2390c3acf5905c52e8680f516892d30e3fe
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-21 13:12:14 +0200

    template for detail

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,22 @@
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question': question
+    }
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
