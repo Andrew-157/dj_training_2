db1d31668d56215574ca036e663c652315ca7880
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-21 11:27:53 +0200

    Some changes in models

--- polls/models.py
+++ polls/models.py
@@ -11,8 +11,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice = models.CharField(max_length=255)
+    choice_text = models.CharField(max_length=255)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.question
+        return self.choice_text
